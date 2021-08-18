edb4da675804f036b56fd1a7b630be4a05c9c4ae
Sharon (Sean) Rolel <srolel@coursera.org>
2021-08-18 18:30:58 -0400

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,51 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const {context} = github
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // This should be a token with access to your repository scoped in as a secret.
+    // The YML workflow will need to set myToken with the GitHub Secret Token
+    // myToken: ${{ secrets.GITHUB_TOKEN }}
+    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+    const myToken = core.getInput('myToken')
 
-    core.setOutput('time', new Date().toTimeString())
+    const octokit = github.getOctokit(myToken)
+
+    const {
+      data: {default_branch}
+    } = await octokit.rest.repos.get({
+      ...context.repo
+    })
+
+    const {
+      data: {commit: defaultBranchCommit}
+    } = await octokit.rest.repos.getBranch({
+      ...context.repo,
+      branch: default_branch
+    })
+
+    const defaultBranchCommitDate =
+      defaultBranchCommit.commit.committer?.date ||
+      defaultBranchCommit.commit.author?.date
+
+    core.debug(
+      `${default_branch} branch sha ${defaultBranchCommit.sha} commit date: ${defaultBranchCommitDate}`
+    )
+
+    const {data: currentBranchCommit} = await octokit.rest.repos.getCommit({
+      ...context.repo,
+      ref: context.ref
+    })
+
+    const currentBranchCommitDate =
+      currentBranchCommit.commit.committer?.date ||
+      currentBranchCommit.commit.author?.date
+
+    core.debug(
+      `sha ${currentBranchCommit.sha} commit date: ${currentBranchCommitDate}`
+    )
   } catch (error) {
     core.setFailed(error.message)
   }
